bb6ac129ffd9dcfdd12da2f7df6cc5830c9a781a
Chad Armstrong <chad@edenwaith.com>
2025-07-15 19:57:50 -0600

    ✨ Added Chart View

--- EGA Color Palette/EGA Color Palette/Views/ColorPaletteChartView.swift
+++ EGA Color Palette/EGA Color Palette/Views/ColorPaletteChartView.swift
@@ -13,8 +13,33 @@ struct ColorPaletteChartView: View {
     var colors: OrderedDictionary<String, String>
     
     // Drawing an arc: https://codingwithrashid.com/how-to-create-arc-shapes-in-swiftui/
+    
     var body: some View {
-        Text("Hello, Chart View!")
+        ZStack {
+            
+            let lightGrey:Color = Color(hex: "f2f2f7") ?? Color.gray
+            
+            Rectangle()
+                .fill(lightGrey.gradient)
+                .ignoresSafeArea(edges: .top)
+                    
+            ZStack {
+                
+                let colorsCount: Int = colors.count
+                let hexColors = colors.keys
+                
+                ForEach(0..<colorsCount) { i in
+                    
+                    Circle()
+                        .trim(from: CGFloat(i) / CGFloat(colorsCount),
+                                to: CGFloat(i + 1) / CGFloat(colorsCount))
+                        .stroke(Color(hex: hexColors[i % colorsCount]) ?? Color.gray, lineWidth: 40)
+                        .rotationEffect(.degrees(-90))
+                }
+            }
+            .frame(width: 200, height: 200)
+            .shadow(radius: 2.0)
+        }
     }
 }
 
--- README.md
+++ README.md
@@ -3,5 +3,11 @@ SwiftUI Project Displaying 16 Glorious EGA Colors
 
 Display the traditional 16 EGA colors in a list with their three RGB components.
 
+### List View
+
 <img width="1206" height="2622" alt="EGA-Color-Palette-List" src="EGA-Color-Palette-List.png" />
+
+### Chart View
+
+<img width="1206" height="2622" alt="EGA-Color-Palette-List" src="EGA-Color-Palette-Chart.png" />
 
